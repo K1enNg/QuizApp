c584bce2647c29d11f7ec07ebd2ae4ff6a16bb4e
Kinkin <hiimkien2702@gmail.com>
2025-02-23 02:37:50 -0500

    generated questions

---
+++ QuizApp/Services/QuizService.cs
@@ -0,0 +1,31 @@
+﻿using QuizApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp.Services
+{
+    class QuizService
+    {
+        public List<Question> GetQuestions()
+        {
+            return new List<Question>
+            {
+                new Question
+                {
+                    Text = "What is the capital of France?",
+                    Options = new List<string> {"Berlin", "Madrid", "Paris", "Rome"},
+                    correctAnswerIndex = 2
+                },
+                new Question
+                {
+                    Text = "Which planet is known as the Red Planet?",
+                    Options = new List<string> {"Earth", "Mars", "Venus", "Jupiter"},
+                    correctAnswerIndex = 1
+                }
+            };
+        }
+    }
+}
